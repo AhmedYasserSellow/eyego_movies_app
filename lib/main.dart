2f26840c8348e4e5ae76e8f32499fb0a1c7a6ed9
Ahmed Yasser <aaysr201678@gmail.com>
2025-04-06 12:04:57 +0200

    add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:eyego_movies_app/core/helper/on_generate_routes.dart';
 import 'package:eyego_movies_app/core/utils/app_colors.dart';
 import 'package:eyego_movies_app/features/splash/presentation/views/splash_view.dart';
+import 'package:eyego_movies_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
