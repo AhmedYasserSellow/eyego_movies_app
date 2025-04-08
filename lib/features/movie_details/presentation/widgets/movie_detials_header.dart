8866d176cc3ea97d7a39cd2b0b5a4c721e04bb22
Ahmed Yasser <aaysr201678@gmail.com>
2025-04-08 05:05:19 +0200

    fix backdrop size

--- lib/features/movie_details/presentation/widgets/movie_detials_header.dart
+++ lib/features/movie_details/presentation/widgets/movie_detials_header.dart
@@ -13,55 +13,62 @@ class MovieDetailsHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(kBorderRadius),
-            bottomRight: Radius.circular(kBorderRadius),
-          ),
-          child: CachedNetworkImage(imageUrl: movie.backdropPath),
-        ),
-        Positioned(
-          bottom: 12,
-          right: 20,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-            decoration: BoxDecoration(
-              color: Color(0xff252836),
-              borderRadius: BorderRadius.circular(kBorderRadius),
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).width / (3840 / 2160),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(kBorderRadius),
+              bottomRight: Radius.circular(kBorderRadius),
             ),
-
-            child: CustomListTile(
-              icon: AppIcons.star,
-              title: movie.voteAverage.toString(),
-              color: Color(0xffFFC107),
-            ),
+            child: CachedNetworkImage(imageUrl: movie.backdropPath),
           ),
-        ),
-        Positioned(
-          left: 32,
-          bottom: -60,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ClipRRect(
+          Positioned(
+            bottom: 12,
+            right: 20,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                color: Color(0xff252836),
                 borderRadius: BorderRadius.circular(kBorderRadius),
-                child: CachedNetworkImage(
-                  imageUrl: movie.posterPath,
-                  width: 100,
-                ),
               ),
-              SizedBox(width: 8),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16),
-                child: Text(movie.title, style: AppTextStyles.regularTextStyle),
+
+              child: CustomListTile(
+                icon: AppIcons.star,
+                title: movie.voteAverage.toString(),
+                color: Color(0xffFFC107),
               ),
-            ],
+            ),
           ),
-        ),
-      ],
+          Positioned(
+            left: 32,
+            bottom: -60,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(kBorderRadius),
+                  child: CachedNetworkImage(
+                    imageUrl: movie.posterPath,
+                    width: 100,
+                  ),
+                ),
+                SizedBox(width: 8),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Text(
+                    movie.title,
+                    style: AppTextStyles.regularTextStyle,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
